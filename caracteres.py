1cf7a186080e6802eb65dab64b37511c45ecffd3
pulidovpe <pulidovpe.dev@gmail.com>
2020-05-19 19:25:58 -0400

    Se crea otro test para brobar otro metodo

--- caracteres.py
+++ caracteres.py
@@ -3,3 +3,7 @@ class Caracteres:
 	def mayusculas(self, palabra):
 
 		return palabra.upper()
+
+	def ultimos_4(self, palabra):
+
+		return palabra[-4:]
--- test_caracteres.py
+++ test_caracteres.py
@@ -7,3 +7,10 @@ car = Caracteres()
 
 def test_mayusculas():
     assert car.mayusculas('semaforo') == 'SEMAFORO'
+
+def test_ultimos_4_caracteres():
+    assert car.ultimos_4('pytest') == 'test'
+
+# def test_raises_exception_usando_numeros_en_vez_de_caracteres():
+#     with pytest.raises(TypeError):
+#         car.mayusculas(123)
