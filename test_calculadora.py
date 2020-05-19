2cc2434ff8245a72ef1d6e3ce677c4982c598ec3
pulidovpe <pulidovpe.dev@gmail.com>
2020-05-19 17:45:09 -0400

    Se optimizo el codigo de los tests

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,19 +3,20 @@ from calculadora import Calculadora
 
 class TestsBasicos(unittest.TestCase):
 
+	def setUp(self):
+
+		self.calc = Calculadora()
+
 	def test_calculadora_inicia_en_cero(self):
 
-		calc = Calculadora()
-		self.assertEqual(0,calc.valor())
+		self.assertEqual(0, self.calc.valor())
 
 	def test_suma_de_2_mas_3_debe_dar_5(self):
 
-		calc = Calculadora()
-		calc.suma(2,3)
-		self.assertEqual(5,calc.valor())
+		self.calc.suma(2,3)
+		self.assertEqual(5, self.calc.valor())
 
 	def test_suma_de_4_mas_3_debe_dar_7(self):
 
-		calc = Calculadora()
-		calc.suma(4,3)
-		self.assertEqual(7,calc.valor())
+		self.calc.suma(4,3)
+		self.assertEqual(7, self.calc.valor())
